f97e133a2d0ea7667572c9b40684f592828858d3
naohiko560 <66761935+naohiko560@users.noreply.github.com>
2023-05-25 08:05:35 +0900

    修正

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.TWITTERDRAFT_FIREBASE_apiKey,
-  authDomain: TWITTERDRAFT_FIREBASE_authDomain,
-  projectId: TWITTERDRAFT_FIREBASE_projectId,
-  storageBucket: TWITTERDRAFT_FIREBASE_storageBucket,
-  messagingSenderId: TWITTERDRAFT_FIREBASE_messagingSenderId,
-  appId: TWITTERDRAFT_FIREBASE_appId,
-  measurementId: TWITTERDRAFT_FIREBASE_measurementId,
+  authDomain: process.env.TWITTERDRAFT_FIREBASE_authDomain,
+  projectId: process.env.TWITTERDRAFT_FIREBASE_projectId,
+  storageBucket: process.env.TWITTERDRAFT_FIREBASE_storageBucket,
+  messagingSenderId: process.env.TWITTERDRAFT_FIREBASE_messagingSenderId,
+  appId: process.env.TWITTERDRAFT_FIREBASE_appId,
+  measurementId: process.env.TWITTERDRAFT_FIREBASE_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
