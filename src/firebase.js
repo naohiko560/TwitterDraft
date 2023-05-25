c48f46b253fe493cf25088e489ce48e8cabc67ab
naohiko560 <66761935+naohiko560@users.noreply.github.com>
2023-05-25 10:10:14 +0900

    firebase.jsファイルのコメントアウト

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-import { getAuth } from 'firebase/auth';
-import { GoogleAuthProvider } from 'firebase/auth';
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// import { getAuth } from 'firebase/auth';
+// import { GoogleAuthProvider } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: process.env.TWITTERDRAFT_FIREBASE_apiKey,
-  authDomain: process.env.TWITTERDRAFT_FIREBASE_authDomain,
-  projectId: process.env.TWITTERDRAFT_FIREBASE_projectId,
-  storageBucket: process.env.TWITTERDRAFT_FIREBASE_storageBucket,
-  messagingSenderId: process.env.TWITTERDRAFT_FIREBASE_messagingSenderId,
-  appId: process.env.TWITTERDRAFT_FIREBASE_appId,
-  measurementId: process.env.TWITTERDRAFT_FIREBASE_measurementId,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.TWITTERDRAFT_FIREBASE_apiKey,
+//   authDomain: process.env.TWITTERDRAFT_FIREBASE_authDomain,
+//   projectId: process.env.TWITTERDRAFT_FIREBASE_projectId,
+//   storageBucket: process.env.TWITTERDRAFT_FIREBASE_storageBucket,
+//   messagingSenderId: process.env.TWITTERDRAFT_FIREBASE_messagingSenderId,
+//   appId: process.env.TWITTERDRAFT_FIREBASE_appId,
+//   measurementId: process.env.TWITTERDRAFT_FIREBASE_measurementId,
+// };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth = getAuth(app);
+// const provider = new GoogleAuthProvider();
 
-export { auth, provider };
+// export { auth, provider };
