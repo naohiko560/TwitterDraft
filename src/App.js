a79fe7cc6a5c1b60590c3e48f32701d11346b5c8
naohiko560 <private.mail.nao@gmail.com>
2024-01-04 21:01:28 +0900

    font調整

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import { Home } from './components/Home';
 function App() {
   return (
     <div className="App">
-      <h1>X（旧Twitter）下書きツール</h1>
-      <Home/>
+      <h1>
+        X（旧Twitter）
+        <br className='sp' />
+        下書きツール
+      </h1>
+      <Home />
     </div>
   );
 }
